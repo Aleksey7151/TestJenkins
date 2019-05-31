cdaa660c27be4dc5e7155b524f0bcc480c267801
aleksey <Aliaksei_Safonau@epam.com>
2019-05-31 23:21:05 +0300

    Add d==5555

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -9,8 +9,9 @@ namespace SimpleConsoleApp
             var a = 9;
             var b = 33;
             var c = 444;
+            var d = 5555;
             Console.WriteLine($"Hello World! {a + b}");
-            Console.WriteLine($"Hello World! {c}");
+            Console.WriteLine($"Hello World! {c + d}");
         }
     }
 }
